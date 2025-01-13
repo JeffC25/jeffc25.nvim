a15bc3cfcc6b6b0765dea1efe1aa49ccdc6b022d
JeffC25 <34695547+JeffC25@users.noreply.github.com>
2025-01-13 03:38:18 -0500

    treesitter

--- lua/jeffc25/plugins/treesitter.lua
+++ lua/jeffc25/plugins/treesitter.lua
@@ -1,3 +1,23 @@
-return { {} }
+-- Highlight, edit, and navigate code
+
+return {
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
+    -- See `:help nvim-treesitter`
+    opts = {
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'go' },
+      -- Autoinstall languages that are not installed
+      auto_install = true,
+      highlight = {
+        enable = true,
+        -- Some languages depend on vim's regex highlighting system (Ex: Ruby) for indent rules.
+        additional_vim_regex_highlighting = { 'ruby' },
+      },
+      indent = { enable = true, disable = { 'ruby' } },
+    },
+  },
+}
 
 -- vim: ts=2 sts=2 sw=2 et
