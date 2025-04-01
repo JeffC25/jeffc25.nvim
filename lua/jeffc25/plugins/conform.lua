2b941a6f9ad428130e97919ff4ce42b6846dbf75
JeffC25 <34695547+JeffC25@users.noreply.github.com>
2025-03-31 21:24:19 -0400

    disable auto format on save

--- lua/jeffc25/plugins/conform.lua
+++ lua/jeffc25/plugins/conform.lua
@@ -17,27 +17,12 @@ return {
     },
     opts = {
       notify_on_error = false,
-      format_on_save = function(bufnr)
-        -- Disable "format_on_save lsp_fallback" for languages without standardized coding style.
-        local disable_filetypes = { c = true, cpp = true }
-        local lsp_format_opt
-        if disable_filetypes[vim.bo[bufnr].filetype] then
-          lsp_format_opt = 'never'
-        else
-          lsp_format_opt = 'fallback'
-        end
-        return {
-          timeout_ms = 500,
-          lsp_format = lsp_format_opt,
-        }
-      end,
       formatters_by_ft = {
         lua = { 'stylua' },
         python = {
           'isort',
           -- 'black'
         },
-
         javascript = { 'prettier', stop_after_first = true },
         javascriptreact = { 'prettier' },
         typescript = { 'prettier' },
@@ -55,7 +40,6 @@ return {
         handlebars = { 'prettier' },
         json = { 'fixjson', 'prettier' },
         xml = { 'xmlformatter' },
-
         go = { 'gofumpt', 'goimports' },
       },
     },
