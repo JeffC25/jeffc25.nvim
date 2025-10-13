d225ff596a3dd294316e45050cc2e29ba59861e9
JeffC25 <34695547+JeffC25@users.noreply.github.com>
2025-10-13 15:13:52 -0400

    back to sonokai

--- lua/jeffc25/plugins/theme.lua
+++ lua/jeffc25/plugins/theme.lua
@@ -47,7 +47,7 @@ return {
     'f-person/auto-dark-mode.nvim',
     opts = {
       set_dark_mode = function()
-        vim.cmd.colors('carbonfox')
+        vim.cmd.colors('sonokai')
       end,
       set_light_mode = function()
         vim.cmd.colors('season')
