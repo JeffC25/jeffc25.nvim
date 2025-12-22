1594d263a637f953ce081da7bdfaa2794495ece7
JeffC25 <34695547+JeffC25@users.noreply.github.com>
2025-12-22 18:09:37 -0500

    configure folds, update flash bindings

--- lua/jeffc25/plugins/flash.lua
+++ lua/jeffc25/plugins/flash.lua
@@ -1,13 +1,28 @@
 return {
   "folke/flash.nvim",
-  event = "VeryLazy",
+  lazy = false, -- load immediately
   ---@type Flash.Config
-  opts = {},
+  opts = {
+    modes = {
+      char = {
+        enabled = false,  -- disable default char mode so f/F don't stop at 1 char
+      },
+      search = {
+        enabled = true,   -- enable multi-character search
+        jump_labels = true,
+      },
+    },
+  },
   keys = {
-    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-    { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    -- f = multi-char forward search
+    {"f", mode = { "n", "x", "o" }, function() require("flash").jump({ search = { mode = "search" }, }) end, desc = "Flash search forward",},
+    -- F = multi-char backward search
+    {"F", mode = { "n", "x", "o" }, function() require("flash").jump({ search = { mode = "search", backward = true }, }) end, desc = "Flash search backward",},
+    -- Remote flash (operator-pending)
+    {"r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash",},
+    {"R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search",},
+    -- Toggle flash in command-line mode
+    {"<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search",},
   },
 }
+
--- lua/options.lua
+++ lua/options.lua
@@ -64,6 +64,14 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- Folds
+vim.o.foldenable = true
+vim.o.foldlevel = 99
+vim.o.foldmethod = "expr"
+vim.o.foldexpr = 'v:lua.vim.lsp.foldexpr()'
+vim.o.foldtext = ""
+vim.opt.foldcolumn = "0"
+
 -- Update directory when launching
 vim.api.nvim_create_autocmd('VimEnter', {
   callback = function()
