380f6c1ae5e42636c2c6361a72c692e90fb8d147
JeffC25 <34695547+JeffC25@users.noreply.github.com>
2026-02-09 00:12:15 -0500

    add twilight

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -30,6 +30,7 @@ require('lazy').setup({
   require('plugins.ui.todo-comments'),
   require('plugins.ui.icons'),
   require('plugins.ui.indent-guides'),
+  require('plugins.ui.twilight'),
   require('plugins.ui.zen'),
 
   require('plugins.utils.mini'),
