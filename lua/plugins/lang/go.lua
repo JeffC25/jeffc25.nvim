23c800468e1f500a1b45fb1e39de4ea87a7d08be
JeffC25 <34695547+JeffC25@users.noreply.github.com>
2026-02-15 20:27:32 -0500

    numbers jump to harpoon buffer when quick menu is open, hot fix for inlay hints enabling when saving caused by go.lua

--- lua/plugins/lang/go.lua
+++ lua/plugins/lang/go.lua
@@ -6,7 +6,13 @@ return {
     'nvim-treesitter/nvim-treesitter',
   },
   config = function()
-    require('go').setup({})
+    require('go').setup({
+      -- ISSUE: default options enabling inlay hints on save (for non-go files)
+      lsp_inlay_hints = {
+        enable = false,
+      },
+    })
+
     local format_sync_grp = vim.api.nvim_create_augroup('GoFormat', {})
     vim.api.nvim_create_autocmd('BufWritePre', {
       pattern = '*.go',
@@ -15,10 +21,12 @@ return {
       end,
       group = format_sync_grp,
     })
+    return {
+      -- lsp_keymaps = false,
+      -- other options
+    }
   end,
   event = { 'CmdlineEnter' },
   ft = { 'go', 'gomod' },
   build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
 }
-
--- vim: ts=2 sts=2 sw=2 et
