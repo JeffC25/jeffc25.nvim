3ace8fdb7b15910b4c4711c1fbeacabef8f05cd8
JeffC25 <34695547+JeffC25@users.noreply.github.com>
2026-08-30 19:32:10 -0400

    autopair for angle brackets

--- lua/plugins/lsp/autopairs.lua
+++ lua/plugins/lsp/autopairs.lua
@@ -3,6 +3,15 @@
 local pack = require('pack')
 pack.add({ pack.gh('windwp/nvim-autopairs') })
 
-require('nvim-autopairs').setup()
+local npairs = require('nvim-autopairs')
+local Rule = require('nvim-autopairs.rule')
+
+npairs.setup()
+
+npairs.add_rule(Rule('<', '>'):with_pair(function(opts)
+  local line = opts.line
+  local col = opts.col
+  return line:sub(col - 1, col - 1):match('[%w_]') ~= nil
+end))
 
 -- vim: ts=2 sts=2 sw=2 et
