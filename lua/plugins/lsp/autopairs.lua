148c2abcfea166273cbdd48cf67f31b3a2d3d9c8
JeffC25 <34695547+JeffC25@users.noreply.github.com>
2026-08-31 00:22:16 -0400

    Revert "autopair for angle brackets"
    This reverts commit 3ace8fdb7b15910b4c4711c1fbeacabef8f05cd8.

--- lua/plugins/lsp/autopairs.lua
+++ lua/plugins/lsp/autopairs.lua
@@ -3,15 +3,6 @@
 local pack = require('pack')
 pack.add({ pack.gh('windwp/nvim-autopairs') })
 
-local npairs = require('nvim-autopairs')
-local Rule = require('nvim-autopairs.rule')
-
-npairs.setup()
-
-npairs.add_rule(Rule('<', '>'):with_pair(function(opts)
-  local line = opts.line
-  local col = opts.col
-  return line:sub(col - 1, col - 1):match('[%w_]') ~= nil
-end))
+require('nvim-autopairs').setup()
 
 -- vim: ts=2 sts=2 sw=2 et
